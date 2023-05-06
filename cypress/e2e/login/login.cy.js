142a19848fea37f230c39f074f4f97aa451bf32c
Gabriel Bento <gabrielbento299@hotmail.com>
2023-05-06 12:18:11 -0300

    first commit

---
+++ cypress/e2e/login/login.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('Teste funcional de login', () => {
+    beforeEach(() => {
+        cy.visit('https://www.saucedemo.com/');
+    });
+
+    it('Deve realizar o login com sucesso', () => {
+
+    });
+});
