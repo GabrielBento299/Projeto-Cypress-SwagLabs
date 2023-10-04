4fc2e75fbc5da28d57ab4b68f924b6a63443f892
Gabriel Bento <gabrielbento299@hotmail.com>
2023-10-04 10:40:24 -0300

    Created only one commands

--- cypress/support/cadastroInfosCommands.js
+++
@@ -1,7 +0,0 @@
-Cypress.Commands.add('cadastroInfos', () => {
-    cy.get('[data-test="firstName"]').type('Teste');
-    cy.get('[data-test="lastName"]').type('Teste da Silva');
-    cy.get('[data-test="postalCode"]').type('123456');
-    
-    cy.get('[data-test="continue"]').click();
-});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,42 @@
+Cypress.Commands.add('login', (email, password) => {
+    cy.visit('/');
+
+    if (email) {
+        cy.get('[data-test="username"]').type(email);
+    }
+
+    if (password) {
+        cy.get('[data-test="password"]').type(password);    
+    }
+ 
+    cy.get('[data-test="login-button"]').click();  
+});
+
 Cypress.Commands.add('logout', () => {
     cy.get('.bm-burger-button').click();
     cy.contains('.bm-item-list a', 'Logout').click();
+});
+
+Cypress.Commands.add('cadastroInfos', () => {
+    cy.get('[data-test="firstName"]').type('Teste');
+    cy.get('[data-test="lastName"]').type('Teste da Silva');
+    cy.get('[data-test="postalCode"]').type('123456');
+    
+    cy.get('[data-test="continue"]').click();
+});
+
+Cypress.Commands.add('addProductCart', () => {
+    cy.get('.inventory_item')
+        .last().children()
+        .find('img').click();
+
+    cy.get('.btn_primary').click();
+    cy.get('.inventory_details_desc_container .btn_secondary').should('have.text', 'Remove');
+
+});
+
+Cypress.Commands.add('addAllProductsCart', () => {
+    cy.get('.btn_primary').each(($el) => {
+        cy.wrap($el).click();
+    });
 });
