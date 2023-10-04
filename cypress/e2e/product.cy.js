fc4b2aa05cc4b999e8da809f0b1d7d486025f7a1
Gabriel Bento <gabrielbento299@hotmail.com>
2023-10-04 11:06:29 -0300

    Translated test and variables to english

---
+++ cypress/e2e/product.cy.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+
+describe('Products Test', () => {
+    beforeEach(() => {
+        cy.login('standard_user', 'secret_sauce');
+    });
+
+    it('should successfully purchase a product', () => {
+        cy.addProductCart();
+
+        cy.get('.shopping_cart_link').click();
+        
+        cy.get('[data-test="checkout"]').click();
+
+        cy.registrationInfos();
+
+        cy.get('.summary_total_label').should('have.text', 'Total: $17.27');
+        cy.get('[data-test="finish"]').click();
+        
+        cy.get('.title').contains('Checkout: Complete!').should('be.visible');
+        cy.get('.complete-header')
+            .should('be.visible')
+            .and('contain', 'Thank you for your order!');
+    });
+
+    it('should successfully purchase all product', () => {
+        cy.addAllProductsCart();
+
+        cy.get('.shopping_cart_link').click();
+        cy.get('[data-test="checkout"]').click();
+
+        cy.registrationInfos();
+
+        cy.get('.summary_total_label').should('have.text', 'Total: $140.34');
+        cy.get('[data-test="finish"]').click();
+
+        cy.get('.title').contains('Checkout: Complete!').should('be.visible');
+        cy.get('.complete-header')
+            .should('be.visible')
+            .and('contain', 'Thank you for your order!');
+    });
+});
